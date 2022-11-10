65be9b336f3a62cc8df93f0aab8eff4c2080aef5
Stephen Wild <stephen.james.wild@gmail.com>
2022-11-09 20:38:02 -0500

    should work now

--- hurdle_ordered_function.R
+++ hurdle_ordered_function.R
@@ -3,18 +3,21 @@
 
 cmstanr_to_brms <- function(
   .formula, 
-  .nthres = NULL, 
-  .DK,
+  .outcome = NULL, 
+  #.DK,
   .family, .priors, .data, .chains, .cores = parallel::detectCores(logical = F),
   .warmup, 
   #.stan_vars = NULL,
-  .sampling, .seed, .base_name = "/cmdtanr_file", .base_dir = getwd(), .dest = NULL, 
+  .sampling, .seed, .base_name = "/cmdtanr_file", .base_dir = here(), .dest = NULL, 
   .stan_dir = str_c(.base_dir, "/stan"), .metadata = FALSE, ...
 ) {
   
-  if(is.null(.nthres)){
-    print("You must manually set the number of thresholds")
+  if(is.null(.outcome)){
+    stop("You must input the outcome. Do not forget to set DK to be one higher than the scale")
   }
+  
+  nthres <- length(unique(.outcome)) - 2
+  DK <- max(.outcome)
   
   # stan funs for custom family
   stan_funs <- "
@@ -42,7 +45,7 @@ cmstanr_to_brms <- function(
   
   "
   # stan_vars for custom model
-  stan_vars <- stanvar(scode = stan_funs, block = "functions") #+ stanvar(scode = paste0("int DK = ", .DK), block = "tdata")
+  stan_vars <- stanvar(scode = stan_funs, block = "functions") 
   
   
   # Generate a temporary brmsfit object
@@ -69,13 +72,13 @@ cmstanr_to_brms <- function(
     stanvars = stan_vars
   )
   
-  stan_data$nthres <- .nthres
+  stan_data$nthres <- nthres
   stan_code <- stancode(brmsfit_fit)
-  stan_code <- gsub("y == 99", paste0("y == ", .DK), stan_code)
-  stan_code <- gsub("return 0;", paste0("return ", .DK, ";"), stan_code)
+  stan_code <- gsub("y == 99", paste0("y == ", DK), stan_code)
+  stan_code <- gsub("return 0;", paste0("return ", DK, ";"), stan_code)
   
   
-  brmsfit_fit$family$thres <- data.frame(thres = 1:.nthres,
+  brmsfit_fit$family$thres <- data.frame(thres = 1:nthres,
                                          #group = as.character(c(1:(.nthres))),
                                          group = ""
   )
@@ -156,51 +159,23 @@ hurdle_ordinal <-
                 type = "int",
                 threshold = "flexible")
 
-# Stan code
-# here comes the fun
-stan_funs <- "
-  real hurdle_ordinal_lpmf(int y, real mu, real hu, vector c) { 
-  //real hurdle_ordinal_lpmf(int y, vector mu, vector hu, vector c, vint1 DK) { 
-
-    if (y == 99) { 
-      return bernoulli_logit_lpmf(1 | hu); 
-    } else { 
-      return bernoulli_logit_lpmf(0 | hu) +  
-             ordered_logistic_lpmf(y | mu, c); 
-    } 
-  }
-  
-  int hurdle_ordinal_rng(real hu, real mu, vector c) {
-    real p = inv_logit(hu);
-    if (bernoulli_rng(p) == 1){
-      return DK;
-    }
-    else {
-      return ordered_logistic_rng(mu, c);
-    }
-  }
-  
-"
-
-stan_var <- stanvar(scode = stan_funs, block = "functions") #+ stanvar(scode = paste0("int DK = ", .DK), block = "tdata")
-
 log_lik_hurdle_ordinal <- function(i, prep) {
   mu <- get_dpar(prep, "mu", i = i)
-  hu <- inv_logit(get_dpar(prep, "mu", i = i))
+  hu <- inv_logit(get_dpar(prep, "hu", i = i))
   thres <- subset_thres(prep, i)
   nthres <- NCOL(thres)
   eta <- thres - mu
   y <- prep$data$Y[i]
+  DK <- max(prep$data$Y)
   if (y == 1L) {
     out <- log_cdf(eta[, 1L], "logit") + 
       dbinom(0, size = 1, prob = hu, log = TRUE)
   } else if (y == nthres + 1L) {
     out <- log_ccdf(eta[, y - 1L], "logit") + 
       dbinom(0, size = 1, prob = hu, log = TRUE)
-  } else if (y == nthres + 2L) {
-    dbinom(1, size = 1, prob = hu, log = TRUE)
-  }
-  else {
+  } else if (y == DK) {
+    out <- dbinom(1, size = 1, prob = hu, log = TRUE)
+  } else {
     out <- log_diff_exp(
       log_cdf(eta[, y], "logit"),
       log_cdf(eta[, y - 1L], "logit") 
@@ -215,7 +190,7 @@ posterior_predict_hurdle_ordinal <- function(i, prep, ...) {
   thres <- subset_thres(prep)
   nthres <- NCOL(thres)
   ndraws <- prep$ndraws
-  DK <- as.numeric(max(prep$cats))
+  DK <- as.numeric(max(prep$data$Y))
   p <- pordinal(
     seq_len(nthres + 1),
     eta = get_dpar(prep, "mu", i = i),
@@ -231,7 +206,6 @@ posterior_predict_hurdle_ordinal <- function(i, prep, ...) {
 }
 
 
-
 posterior_epred_hurdle_ordinal <- function(prep) {
   mu <- brms::get_dpar(prep, "mu")
   hu <- brms::get_dpar(prep, "hu")
@@ -239,184 +213,3 @@ posterior_epred_hurdle_ordinal <- function(prep) {
 }
 
 
-
-out <- cmstanr_to_brms(
-  .formula = bf(y ~ X1 + X2 + X3 + X4 + X5,
-     hu ~ 0 + X1 + X2 + X3 + X4 + X5),
-  .nthres = 2, 
-  .DK = 4,
-  .family = hurdle_ordinal,
-  .prior = c(prior(normal(0, 2), class = b)),
-  .data = df,
-  .chains = 4,
-  .cores = 4,
-  .warmup = 1000,
-  .sampling = 1000,
-  .seed = 1234
-)
-
-expose_functions(out, vectorize = TRUE)
-
-
-get_thres <- function(prep){
-  return(prep$thres$thres)
-}
-
-first_greater <- function(A, target, i = 1) {
-  ifelse(target <= A[, i] | ncol(A) == i, i, first_greater(A, target, i + 1))
-}
-
-pordinal <- function(q, eta, thres, disc = 1, family = NULL, link = "logit") {
-  family <- as_one_character(family)
-  link <- as_one_character(link)
-  args <- nlist(x = seq_len(max(q)), eta, thres, disc, link)
-  p <- do_call(paste0("d", family), args)
-  .fun <- function(j) rowSums(as.matrix(p[, 1:j, drop = FALSE]))
-  cblapply(q, .fun)
-}
-
-cblapply <- function(X, FUN, ...) {
-  do.call(cbind, lapply(X, FUN, ...))
-}
-
-as_one_character <- function(x, allow_na = FALSE) {
-  s <- substitute(x)
-  x <- as.character(x)
-  if (length(x) != 1L || anyNA(x) && !allow_na) {
-    s <- deparse_combine(s, max_char = 100L)
-    stop2("Cannot coerce '", s, "' to a single character value.")
-  }
-  x
-}
-
-nlist <- function(...) {
-  m <- match.call()
-  dots <- list(...)
-  no_names <- is.null(names(dots))
-  has_name <- if (no_names) FALSE else nzchar(names(dots))
-  if (all(has_name)) return(dots)
-  nms <- as.character(m)[-1]
-  if (no_names) {
-    names(dots) <- nms
-  } else {
-    names(dots)[!has_name] <- nms[!has_name]
-  }
-  dots
-}
-
-subset_thres <- function(prep, i) {
-  thres <- prep$thres$thres
-  Jthres <- prep$thres$Jthres
-  if (!is.null(Jthres)) {
-    thres <- thres[, Jthres[i, 1]:Jthres[i, 2], drop = FALSE]
-  }
-  thres
-}
-
-dcumulative <- function(x, eta, thres, disc = 1, link = "logit") {
-  eta <- disc * (thres - eta)
-  if (link == "identity") {
-    out <- eta
-  } else {
-    out <- inv_link_cumulative(eta, link = link)
-  }
-  out[, x, drop = FALSE]
-}
-
-inv_link_cumulative <- function(x, link) {
-  x <- inv_link(x, link)
-  ndim <- length(dim(x))
-  dim_noncat <- dim(x)[-ndim]
-  ones_arr <- array(1, dim = c(dim_noncat, 1))
-  zeros_arr <- array(0, dim = c(dim_noncat, 1))
-  abind::abind(x, ones_arr) - abind::abind(zeros_arr, x)
-}
-
-
-posterior_predict_ordinal <- function(i, prep, ...) {
-  thres <- subset_thres(prep, i)
-  nthres <- NCOL(thres)
-  p <- pordinal(
-    seq_len(nthres + 1),
-    eta = get_dpar(prep, "mu", i = i),
-    disc = get_dpar(prep, "disc", i = i),
-    thres = thres,
-    family = prep$family$family,
-    link = prep$family$link
-  )
-  first_greater(p, target = runif(prep$ndraws, min = 0, max = 1))
-}
-
-
-inv_link <- function(x, link) {
-  switch(link,
-         identity = x,
-         log = exp(x),
-         logm1 = expp1(x),
-         log1p = expm1(x),
-         inverse = 1 / x,
-         sqrt = x^2,
-         "1/mu^2" = 1 / sqrt(x),
-         tan_half = 2 * atan(x),
-         logit = inv_logit(x),
-         probit = pnorm(x),
-         cauchit = pcauchy(x),
-         cloglog = inv_cloglog(x),
-         probit_approx = pnorm(x),
-         softplus = log1p_exp(x),
-         squareplus = (x + sqrt(x^2 + 4)) / 2,
-         softit = inv_softit(x),
-         stop2("Link '", link, "' is not supported.")
-  )
-}
-
-inv_logit <- function(x) {
-  1 / (1 + exp(-x))
-}
-
-log_diff_exp <- function(x, y) {
-  stopifnot(length(x) == length(y))
-  ifelse(x > y, log(exp(x) - exp(y)), NaN)
-}
-
-
-log_lik_weight <- function(x, i, prep) {
-  weight <- prep$data$weights[i]
-  if (!is.null(weight)) {
-    x <- x * weight
-  }
-  x
-}
-
-log_cdf <- function(x, link) {
-  switch(link,
-         logit = log_inv_logit(x),
-         probit = pnorm(x, log.p = TRUE),
-         cauchit = pcauchy(x, log.p = TRUE),
-         cloglog = log1m_exp(-exp(x)),
-         probit_approx = pnorm(x, log.p = TRUE),
-         softit = log_inv_softit(x),
-         stop2("Link '", link, "' is not supported.")
-  )
-}
-
-log_ccdf <- function(x, link) {
-  switch(link,
-         logit = log1m_inv_logit(x),
-         probit = pnorm(x, log.p = TRUE, lower.tail = FALSE),
-         cauchit = pcauchy(x, log.p = TRUE, lower.tail = FALSE),
-         cloglog = -exp(x),
-         probit_approx = pnorm(x, log.p = TRUE, lower.tail = FALSE),
-         softit = log1m_inv_softit(x),
-         stop2("Link '", link, "' is not supported.")
-  )
-}
-
-
-log_inv_logit <- function(x) {
-  log(inv_logit(x))
-}
-
-log1m_inv_logit <- function(x) {
-  log(1 - inv_logit(x))
-}
---
+++ test with data.R
@@ -0,0 +1,95 @@
+library(brms)
+library(cmdstanr)
+library(posterior)
+library(magrittr)
+library(tidyverse)
+library(here)
+
+source("helper_functions_from_brms.R")
+source("hurdle_ordered_function.R")
+
+
+# more complex version
+N <- 10000
+P <- 5
+cutpoints <- c(1, -1)
+y_star <- y <- rep(0, N)
+
+
+set.seed(143140914)
+X <- matrix(runif(N * P), N, P)
+betas <- c(-.15, -.5, 3, .5, -1)
+gammas <- c(3, -2, -2, -1, -.5)
+p2 <- plogis(cutpoints[1] + X %*% betas)
+p3 <- plogis(cutpoints[2] + X %*% betas)
+p1 <- 1 - p2
+p2 <- p2 - p3
+for(i in 1:N){
+  y_star[i] <- y[i] <- sample(1:3, 
+                              size = 1,
+                              prob = c(p1[i], p2[i], p3[i]))
+}
+
+
+# Simulate non-response bias
+p <- plogis(X %*% gammas)
+dk <- rbinom(N, 1, p)
+y[dk == 1] = 4
+summary(as.factor(y))
+
+df <- data.frame(y = y, X)
+
+
+
+# test function
+out <- cmstanr_to_brms(
+  .formula = bf(y ~ X1 + X2 + X3 + X4 + X5,
+                hu ~ 0 + X1 + X2 + X3 + X4 + X5),
+  .outcome = df$y,
+  .family = hurdle_ordinal,
+  .prior = c(prior(normal(0, 2), class = b)),
+  .data = df,
+  .chains = 4,
+  .cores = 4,
+  .warmup = 1000,
+  .sampling = 1000,
+  .seed = 1234
+)
+
+expose_functions(out, vectorize = TRUE)
+
+
+
+
+summary(out)
+pp_check(out) + theme_minimal()
+loo(out)
+summary(out)
+
+
+
+
+#### test with real data ####
+merkel <- read.csv("https://raw.githubusercontent.com/octmedina/zi-ordinal/main/merkel_data.csv")
+merkel$confid_merkel[merkel$confid_merkel == 0] <- 5
+
+
+
+out_merkel <- cmstanr_to_brms(
+  .formula = bf(confid_merkel ~ edu + race + income + party,
+                hu ~ 1 + edu + race + income + party),
+  .outcome = merkel$confid_merkel,
+  .family = hurdle_ordinal,
+  .prior = c(prior(normal(0, 2), class = b),
+             prior(normal(0, 2), class = Intercept, dpar = hu)),
+  .data = merkel,
+  .chains = 4,
+  .cores = 4,
+  .warmup = 1000,
+  .sampling = 1000,
+  .seed = 1234
+)
+
+summary(out_merkel)
+pp_check(out_merkel, type = "bars") + theme_minimal()
+loo(out_merkel)
